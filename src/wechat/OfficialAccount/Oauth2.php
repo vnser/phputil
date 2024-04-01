0e0301b1eb81a2af4f7418a5caeef3a378a2289c
vring <971626354@qq.com>
2024-04-01 17:19:41 +0800

    增加微信oauth2

--- src/wechat/OfficialAccount/Oauth2.php
+++ src/wechat/OfficialAccount/Oauth2.php
@@ -5,7 +5,7 @@
  * Email: <971626354@qq.com>
  */
 
-namespace vring\wechat;
+namespace vring\wechat\OfficialAccount;
 
 use EasyWeChat\OfficialAccount\Application;
 use vring\util\Url;
@@ -13,9 +13,11 @@ use vring\util\Url;
 class Oauth2
 {
     /**
+     * @param Application $app
      * @param string $url 获取到openid后的跳转url，如果有带参数，可先对整个url做下urlencode编码
-     * @param int $flag 0表示静默获取openid，1表示需要用户授权获取详细信息
-     * @return void
+     * @param string $scopes
+     * @return array|void
+     * @throws \Exception
      */
     static public function oauth2(Application $app,string $url,string $scopes = 'snsapi_userinfo')
     {
@@ -24,23 +26,24 @@ class Oauth2
         }
         ;
 //        $aUrl = self::getOauth2Url($url,$flag);
-        $auth_user  = $_SESSION["wachat_user"]??null;
+        $auth_user  = $_SESSION["wechat_user"]??null;
         if ($auth_user){
             return $auth_user;
         }
         if (!isset($_GET['code'])){
-            $rep = $app->oauth->scopes([$scopes])
+            $app->oauth->scopes([$scopes])
 //                ->setRequest($request)
-                ->redirect();
-            print_r($rep);
-//            header('location: '.$aUrl);
+                ->redirect($url)->sendHeaders();
             exit;
+//            return ($rep);
+//            header('location: '.$aUrl);
+//            exit;
         }
         $code = $_GET['code'] ?? '';
         if ($code){
-          /*  if (isset($_GET['userinfo'])){
-                $_GET['userinfo'] = json_decode(base64_decode($_GET['userinfo']),true);
-            }*/
+            /*  if (isset($_GET['userinfo'])){
+                  $_GET['userinfo'] = json_decode(base64_decode($_GET['userinfo']),true);
+              }*/
             $user = $app->oauth->user();
             $_SESSION["wechat_user"] = $user;
             return $user;
