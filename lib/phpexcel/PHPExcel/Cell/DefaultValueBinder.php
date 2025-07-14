84aca2d53b15d76989a41b33bc0f7cb9771fe833
vring <vring@qq.com>
2025-07-14 15:47:20 +0800

    优化excel数据导出

--- lib/phpexcel/PHPExcel/Cell/DefaultValueBinder.php
+++ lib/phpexcel/PHPExcel/Cell/DefaultValueBinder.php
@@ -94,7 +94,7 @@ class PHPExcel_Cell_DefaultValueBinder implements PHPExcel_Cell_IValueBinder
 		} elseif ($pValue instanceof PHPExcel_RichText) {
 			return PHPExcel_Cell_DataType::TYPE_STRING;
 
-		} elseif ($pValue{0} === '=') {
+		} elseif ($pValue[0] === '=') {
 			return PHPExcel_Cell_DataType::TYPE_FORMULA;
 
 		} elseif (is_bool($pValue)) {
